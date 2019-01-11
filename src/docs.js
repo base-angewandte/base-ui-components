f140adcb414baa71996959652476e4f886166840
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2019-01-11 13:28:49 +0100

    store init for demo

--- src/docs.js
+++ src/docs.js
@@ -1,15 +1,28 @@
+/* eslint-disable no-unused-vars */
 import Vue from 'vue';
 import SvgIcon from 'vue-svgicon';
+import Vuex from 'vuex';
+
 import App from './app';
 
 import './styles/app.scss';
 import './styles/main.scss';
+import SkosmosAPI from './store/SkosmosAPI';
+
+
+Vue.use(Vuex);
+const store = new Vuex.Store({ strict: false });
+SkosmosAPI(store, {
+  baseURL: 'https://voc.uni-ak.ac.at/skosmos/rest/v1/',
+}, 'SkosmosAPI');
+
 
 Vue.config.productionTip = false;
 
 Vue.use(SvgIcon, {
   tagName: 'svgicon',
 });
+
 
 /* eslint-disable no-new */
 new Vue({
