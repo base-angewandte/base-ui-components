959991bd6fb7612072f0a5b0df4ec9abc4786308
Sandra Hauzmayer <sandra.hauzmayer@platomics.com>
2019-04-11 11:05:02 +0200

    fix: adding status indicator to upload bar

--- src/components/BaseUploadBar/BaseUploadBar.vue
+++ src/components/BaseUploadBar/BaseUploadBar.vue
@@ -6,14 +6,27 @@
     <div
       :style="{ 'width': progressWidth + '%'}"
       class="base-upload-bar-progress" />
+    <SvgIcon
+      v-if="status === 'success'"
+      class="base-upload-bar-status-icon base-upload-bar-status-icon-success"
+      name="check-mark"/>
+    <SvgIcon
+      v-if="status === 'fail'"
+      class="base-upload-bar-status-icon base-upload-bar-status-icon-fail"
+      name="remove"/>
   </div>
 </template>
 
 <script>
+import SvgIcon from 'vue-svgicon';
+
 /**
  * The status bar displayed during file upload
   */
 export default {
+  components: {
+    SvgIcon,
+  },
   props: {
     /**
      * filename that will be displayed in the bar
@@ -28,6 +41,17 @@ export default {
     progress: {
       type: [Number, String],
       default: 0,
+    },
+    /**
+     * indicate the success or fail of an upload <br>
+     *   allowed values: 'sucess' | 'fail' | ''
+     */
+    status: {
+      type: String,
+      default: '',
+      validator(val) {
+        return ['success', 'fail', ''].includes(val);
+      },
     },
   },
   data() {
@@ -71,6 +95,24 @@ export default {
       z-index: 1;
       height: 100%;
       background-color: $uploadbar-color;
+    }
+
+    .base-upload-bar-status-icon {
+      height: $icon-medium;
+      width: $icon-medium;
+      right: $spacing;
+      position: absolute;
+      top: 50%;
+      transform: translate(0, -50%);
+      z-index: 2;
+
+      &.base-upload-bar-status-icon-success {
+        fill: #33cc33;
+      }
+
+      &.base-upload-bar-status-icon-fail {
+        fill: red;
+      }
     }
   }
 </style>
