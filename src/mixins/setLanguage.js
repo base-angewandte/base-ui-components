6f5881dff68db11dbd7d84a769806601a3052195
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-06-11 15:56:50 +0200

    fix: cut off drop down header text and labels

--- src/mixins/setLanguage.js
+++ src/mixins/setLanguage.js
@@ -2,13 +2,14 @@
 export const setLanguageMixin = {
   methods: {
     getLangLabel(value, useAny = false) {
+      if (typeof value === 'string') return value;
       if (value && this.language && value[this.language]) {
         return value[this.language];
       }
       if (value && this.language && useAny) {
-        const lang = Object.keys(value).find(key => value[key]);
+        const lang = Object.keys(value).find(key => !!value[key]);
         // return the first one that has content
-        return value[lang];
+        return value[lang] || value[this.language];
       }
       return value;
     },
