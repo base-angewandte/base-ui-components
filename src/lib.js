f1416a2b4a2355c3dac30fc4d10ecb1095406520
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-10-16 16:06:48 +0200

    wip: moving FormFieldCreator

--- src/lib.js
+++ src/lib.js
@@ -22,6 +22,7 @@ import BaseToggle from './components/BaseToggle/BaseToggle';
 import BaseToggleGroup from './components/BaseToggleGroup/BaseToggleGroup';
 import BaseOptions from './components/BaseOptions/BaseOptions';
 import BaseResultBoxSection from './components/BaseResultBoxSection/BaseResultBoxSection';
+import BaseFormFieldCreator from './components/BaseFormFieldCreator/BaseFormFieldCreator';
 import 'normalize.css';
 import './styles/lib.scss';
 
@@ -50,4 +51,5 @@ export {
   BaseToggleGroup,
   BaseOptions,
   BaseResultBoxSection,
+  BaseFormFieldCreator,
 };
