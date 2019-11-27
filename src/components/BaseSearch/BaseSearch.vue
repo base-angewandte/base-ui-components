39935928ebb5d9787eede4b0c6dbb5017228ec9d
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-11-27 14:21:39 +0100

    fix: #953: refactored base search to improve mobile behaviour

--- src/components/BaseSearch/BaseSearch.vue
+++ src/components/BaseSearch/BaseSearch.vue
@@ -16,7 +16,7 @@
       autocomplete="off"
       @focus.prevent="inputFocus"
       @blur="inputBlur"
-      @keyup="onKeyUp"
+      @input="onInput"
       @keydown.enter.prevent="">
     <SvgIcon
       v-if="inputInt"
@@ -101,7 +101,8 @@ export default {
     inputFocus() {
       this.active = true;
     },
-    onKeyUp() {
+    onInput(event) {
+      this.inputInt = event.target.value;
       if (this.input !== this.inputInt) {
         this.$emit('input-change', this.inputInt);
       }
