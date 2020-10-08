7c1364a402e3227fe85a6d627a2cfa998a987126
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2020-10-08 10:56:57 +0200

    delete rest of obsolete files

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  DEV_HOST: 'localhost',
-  DEV_PORT: '8081',
-  DEV_HTTPS: false,
-})
