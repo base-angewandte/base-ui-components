47a892aa4fb6163febfd8fba45cbdebeee9e77b3
Sandra Hauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2021-09-29 14:16:59 +0200

    fix: attempting to fix transform-css bash script for windows

--- build/transform-css.sh
+++ build/transform-css.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# attempt to fix this script on windows
+fixpath() {
+  case "$OSTYPE" in
+    *linux* ) echo "${1//\\/\/}"
+      ;;
+    *ux* ) echo "linux"
+      ;;
+    *darwin* ) echo "${1//\\/\/}" ;;
+    *bsd* ) echo "${1//\\/\/}" ;;
+    *win* ) echo "${1//\//\\}" ;;
+  esac
+}
+
 # make css out of lib.scss file and use postcss autoprefixer
-sass src/styles/lib.scss | postcss --use autoprefixer -o lib.post.css;
+sass $(fixpath 'src/styles/lib.scss') | postcss --use autoprefixer -o lib.post.css;
 # create one file out of lib.post.css and normalize.css and minify
-cleancss -o dist/base-ui-components-common.css node_modules/normalize.css/normalize.css lib.post.css;
+cleancss -o $(fixpath 'dist/base-ui-components-common.css') $(fixpath 'node_modules/normalize.css/normalize.css lib.post.css');
 # remove temp file
 rm lib.post.css;
 
 # add lib.css and normalize.css to the main base-ui-components.css by creating a temp file
-cat dist/base-ui-components-common.css dist/base-ui-components.css > temp;
+cat $(fixpath 'dist/base-ui-components-common.css') $(fixpath 'dist/base-ui-components.css') > temp;
 # rename that temp file to replace base-ui-components.css
-mv temp dist/base-ui-components.css;
-
+mv temp $(fixpath 'dist/base-ui-components.css');
