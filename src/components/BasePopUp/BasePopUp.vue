44c0585d901189597da6a1d963a0c0c2f2b5d929
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2022-01-17 14:01:59 +0100

    feat: #1699: BasePopUp: add property to disable default right button

--- src/components/BasePopUp/BasePopUp.vue
+++ src/components/BasePopUp/BasePopUp.vue
@@ -51,6 +51,7 @@
               :icon="!isLoading ? buttonRightIcon : ''"
               :icon-position="'right'"
               :icon-size="'small'"
+              :disabled="buttonRightDisabled"
               class="base-popup-button"
               @clicked="buttonRight">
               <template
@@ -136,6 +137,13 @@ export default {
     buttonRightIcon: {
       type: String,
       default: 'check-mark',
+    },
+    /**
+     * disable right button
+     */
+    buttonRightDisabled: {
+      type: Boolean,
+      default: false,
     },
     /**
      * if true button loader will be shown
