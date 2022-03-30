base-angewandte/base-ui-components

commit 8dda61f3cd9318f7e295b7422259431b54efccca

fix: bug with mainFilter when updating appliedFilters from outside

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -408,14 +408,12 @@ export default {
     appliedFilters: {
       handler(val) {
         // check if value is different from internal value
-        if (JSON.stringify(val.slice(0, -1)) !== JSON.stringify(this.appliedFiltersInt)) {
+        if (JSON.stringify(val.slice(0, -1)) !== JSON.stringify(this.appliedFiltersInt)
+          || JSON.stringify(this.mainFilter) !== JSON.stringify(val[this.mainFilterIndex])) {
           // if yes - update internal value
           [this.mainFilter, ...this.appliedFiltersInt] = JSON.parse(JSON
             .stringify([val.pop(), ...val]));
           // also check if main filter is different separately!
-        } else if (val && val.length === 1
-          && JSON.stringify(this.mainFilter) !== JSON.stringify(val[this.mainFilterIndex])) {
-          [this.mainFilter] = val;
         }
       },
       immediate: true,

commit e850fcd7bc71c6ab2b0bd6d293d6ca5694d7a8cc

fix: BaseAdvancedSearch: fix bug introduced in commit 8dda61f3

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -408,12 +408,13 @@ export default {
     appliedFilters: {
       handler(val) {
         // check if value is different from internal value
-        if (JSON.stringify(val.slice(0, -1)) !== JSON.stringify(this.appliedFiltersInt)
-          || JSON.stringify(this.mainFilter) !== JSON.stringify(val[this.mainFilterIndex])) {
+        if (val && val.length >= 1
+          && (JSON.stringify(val.slice(0, -1)) !== JSON.stringify(this.appliedFiltersInt)
+          || JSON.stringify(this.mainFilter) !== JSON.stringify(val[this.mainFilterIndex]))) {
           // if yes - update internal value
           [this.mainFilter, ...this.appliedFiltersInt] = JSON.parse(JSON
             .stringify([val.pop(), ...val]));
-          // also check if main filter is different separately!
+          // also check if main filter is different separately
         }
       },
       immediate: true,