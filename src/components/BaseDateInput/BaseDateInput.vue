1c0b5283b1c7918a9cb5ab411b0e77e2f7008a4f
Sandra Hauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2023-07-27 16:46:59 +0200

    change(BaseDateInput): improve ResizeObserver usage and disconnect unneeded observers
    Refs: #2315

--- src/components/BaseDateInput/BaseDateInput.vue
+++ src/components/BaseDateInput/BaseDateInput.vue
@@ -980,7 +980,8 @@ export default {
     }
   },
   beforeDestroy() {
-    if (this.resizeObserver) this.resizeObserver.unobserve(this.$refs.baseDateInput);
+    if (this.resizeObserver) this.resizeObserver.disconnect();
+    if (this.labelAdditionsObserver) this.labelAdditionsObserver.disconnect();
   },
   methods: {
     initObservers() {
@@ -998,10 +999,12 @@ export default {
 
       if (this.showLabel && (this.isSwitchableFormat || this.labelRowSlotsHaveData)) {
         // second observer to trigger label additions width calc as soon as element is rendered
-        const tempLabelAdditionsObserver = new ResizeObserver(debounce(50, (entries) => {
+        const tempLabelAdditionsObserver = new ResizeObserver(debounce(50, (entries, observer) => {
           // only do calc when element is filled
           if (entries[0].contentRect.width > 0) {
             this.calcLabelAdditionsWidth(this.$refs.baseDateInput.clientWidth);
+            // only do this once as soon as elements are rendered - then disconnect!
+            observer.disconnect();
           }
         }));
         tempLabelAdditionsObserver.observe(this.$refs.labelAdditions);
