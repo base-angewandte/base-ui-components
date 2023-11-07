base-angewandte/base-ui-components

commit 2e2016944b9a56a7ded66b1c050f44f87e77094c

prepare(BaseAdvancedSearch): fix display of datetime fields in collapsed row and add info which date/time fields are supported
Refs: #2275

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -208,8 +208,8 @@ export default {
      * for mode 'form' provide a specification for the form fields in the form of [OpenAPi
      *  schema object](https://swagger.io/specification/#schema-object).
      *  see also [BaseForm](BaseForm) `form-field-json` prop - currently all field types except
-     *    `multiline` and `chips-below` are supported. Also field groups are supported but maximum
-     *    nesting level is 1.
+     *    `multiline` and `chips-below` and for date/time fields only single date, date range and date time fields
+     *    are supported. Also field groups are supported but maximum nesting level is 1.
      */
     formFilterList: {
       type: Object,
@@ -1022,7 +1022,13 @@ export default {
               label: chipValue ? chipValue.split('-').reverse().join('.') : '',
             })),
           fieldId,
-          fieldType,
+          // BaseCollapsedRow needs information if date is type daterange, timerange or datetime
+          // so alter to 'date' and 'time' for daterange and timerange respectively and 'datetime'
+          // for datetime.
+          fieldType: Object.keys(values).reduce((prev, key) => {
+            const currentType = key.split('_')[0];
+            return currentType !== prev ? prev + currentType : prev;
+          }, ''),
         };
       }
       // NOT COVERED: multiline and chips below

commit 3b271ad0b1f57be2c4851ef2c120609a13d0fd2a

prepare(BaseAdvancedSearch): fix base collapsed filter row date/time filter removal
broken because of changes in commit 2e201694

Refs: #2275

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -1059,7 +1059,7 @@ export default {
         // date could be string if it is just a single date or an object in all other cases
       }
       // case date field
-      if (fieldType === 'date' && filterData.type === 'object') {
+      if ((fieldType.includes('date') || fieldType.includes('time')) && filterData.type === 'object') {
         const objectProperties = Object.keys(filterData.properties);
         return values.reduce((valueObject, value, index) => ({
           ...valueObject,

src/components/BaseAdvancedSearch/BaseCollapsedFilterRow.vue
@@ -289,7 +289,8 @@ export default {
           this.removeFilters();
           // now check if more than one value is left for the group
         } else if (concatValuesArray.length > 1) {
-          if (values[valueIndex].type === 'date') {
+          // check for special case date and time fields
+          if (values[valueIndex].type.includes('date') || values[valueIndex].type.includes('time')) {
             // for date arrays just remove the label so the order of the date_from and date_to does not
             // get mixed up by removing the complete value
             this.filtersInt[filterIndex].filter_values.values[valueIndex].values[groupIndex].label = '';
@@ -312,7 +313,7 @@ export default {
         // if yes - remove the complete filter
         this.filtersInt.splice(filterIndex, 1);
         // special case date object
-      } else if (fieldType === 'date') {
+      } else if (fieldType.includes('date') || fieldType.includes('time')) {
         if (values.filter(value => hasData(value)).length < 2) {
           // if yes - remove the complete filter
           this.filtersInt.splice(filterIndex, 1);