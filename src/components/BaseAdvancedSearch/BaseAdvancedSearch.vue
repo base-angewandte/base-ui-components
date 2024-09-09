base-angewandte/base-ui-components

commit 95cfe47bb4d09625b30a3a810ce48454469c5bda

fix(BaseSearch, BaseAdvancedSearch): keep is-active prop in sync between BaseSearch and BaseAdvancedSearch
Refs: #2761

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -28,7 +28,7 @@
         : identifierPropertyName.controlledVocabularyOption"
       :label-property-name="useAutocompleteFunctionality ? labelPropertyName.autocompleteOption
         : labelPropertyName.controlledVocabularyOption"
-      :is-active="isActive"
+      :is-active.sync="isActive"
       :set-focus-on-active="false"
       :clearable="false"
       :assistive-text="assistiveText"

src/components/BaseSearch/BaseSearch.vue
@@ -488,6 +488,16 @@ export default {
       },
       immediate: true,
     },
+    isActiveInt(val) {
+      if (val !== this.isActive) {
+        /**
+         * inform parent if is active has changed internally
+         * @event update:is-active
+         * @type {boolean}
+         */
+        this.$emit('update:is-active', val);
+      }
+    },
   },
   methods: {
     onEnter(event) {

commit eed098abe5a727c7da9526595d4645c2b04ef7d4

fix(BaseAdvancedSearch): keeping active in sync causes drop down to stay open
* caused by commit 95cfe47b

Refs: #2761

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -79,7 +79,8 @@
           icon-position="right"
           :class="['base-advanced-search__expand-button',
                    { 'base-button-icon-rotate-180': formOpen }]"
-          @click.native.stop="openAdvancedSearch" />
+          @click.native.stop="openAdvancedSearch"
+          @focusin.native.stop />
       </template>
       <!-- ADVANCED SEARCH FORM (MODE 'FORM') -->
       <template #below>

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -102,6 +102,7 @@
           :class="['base-advanced-search-row__icon-button',
                    { 'base-advanced-search-row__icon-button__date': filter.type.includes('date') }]"
           @keydown.tab="onTab"
+          @focusin.stop.prevent
           @click.stop.prevent="removeFilter">
           <BaseIcon
             :title="getI18nTerm(getLangLabel(advancedSearchText.removeFilter))"