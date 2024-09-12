551f42480e0ccc19a54f0a58800487baa2aacb39
Sandra Hauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2024-09-12 16:57:40 +0200

    fix(BaseDateInput): keyboard tab navigation not possible on firefox
    Refs: #2782

--- src/components/BaseDateInput/BaseDateInput.vue
+++ src/components/BaseDateInput/BaseDateInput.vue
@@ -908,28 +908,6 @@ export default {
       this.emitData();
     },
     /**
-     * when input becomes inactive always also blur input field just in case
-     * @param {boolean} val - the changed fromOpen value
-     */
-    fromOpen(val) {
-      if (!val) {
-        this.$refs.inputFrom.blur();
-      } else {
-        this.toOpen = false;
-      }
-    },
-    /**
-     * when input becomes inactive always also blur input field just in case
-     * @param {boolean} val - the changed toOpen value
-     */
-    toOpen(val) {
-      if (!val && this.$refs.inputTo) {
-        this.$refs.inputTo.blur();
-      } else if (val) {
-        this.fromOpen = false;
-      }
-    },
-    /**
      * watch for changes in input field active variable to keep in sync with parent
      * @param {boolean} val - the changed internal is active variable
      */
@@ -1018,11 +996,18 @@ export default {
      * @param {string} origin - did event emit from 'from' or 'to' date field
      */
     onInputBlur(event, origin) {
+      const relatedTargetInput = event.relatedTarget?.parentElement ? event.relatedTarget.parentElement
+        .getElementsByTagName('input') : null;
       // so since these arrows only navigate between input fields we check if there is a
       // related target and if this related target is an input field and if yes we make sure
       // the id is different from the input id of this component (the one the event originated from)
-      if (event.relatedTarget && event.relatedTarget.tagName === 'INPUT'
-        && (!event.relatedTarget.id || event.relatedTarget.id !== event.target.id)) {
+      if (event.relatedTarget
+        && ((event.relatedTarget.tagName === 'INPUT'
+            && (!event.relatedTarget?.id || event.relatedTarget.id !== event.target.id))
+          // additionally also set input active false when the BaseInput 'remove' button
+          // (displayed if `clearable` is true) is triggered in the other date field of the range
+          || (event.relatedTarget?.className === 'base-input__remove-icon-wrapper'
+            && relatedTargetInput && relatedTargetInput[0]?.id !== event.target.id))) {
         // set input active state false
         this[`${origin}Open`] = false;
       }
