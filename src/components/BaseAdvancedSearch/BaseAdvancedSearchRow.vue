base-angewandte/base-ui-components

commit 63fbf73ba422b4de30391ed341cecd01f3b6bafc

change(BaseAdvancedSearch)!: move properties solely purposed for assistive technologies from prop `advancedSearchText` to `assistiveText`
* for unified behaviour across all components
* leave `advancedSearchText` only for visible textual information
* affected properties:  `addFilter`, `removeFilter`, `SelectFilterLabel`, `searchLabel`

BREAKING-CHANGE: move properties `addFilter`, `removeFilter`, `SelectFilterLabel` and `searchLabel` from prop `advancedSearchText` to `assistiveText`. Also the option to use an key to a localization json file instead of a pure text string was removed.

Refs: #2827

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -15,7 +15,7 @@
       v-model="currentInput"
       :show-pre-input-icon="isMainSearch
         && filter[identifierPropertyName.filter] === defaultFilter[identifierPropertyName.filter]"
-      :label="getI18nTerm(getLangLabel(advancedSearchText.searchLabel))"
+      :label="assistiveText.searchLabel"
       :type="searchType"
       :selected-chips.sync="selectedOptions"
       :loadable="filter.type === 'text' || filter.type === 'chips'"
@@ -73,7 +73,7 @@
           :use-form-field-styling="false"
           :show-input-border="false"
           :show-label="false"
-          :label="getI18nTerm(getLangLabel(advancedSearchText.selectFilterLabel))"
+          :label="assistiveText.selectFilterLabel"
           :language="language"
           :drop-down-list-id="'filter-options-' + internalRowId"
           :identifier-property-name="identifierPropertyName.filter"
@@ -120,7 +120,7 @@
           @focusin.stop.prevent
           @click.stop.prevent="removeFilter">
           <BaseIcon
-            :title="getI18nTerm(getLangLabel(advancedSearchText.removeFilter))"
+            :title="assistiveText.removeFilter"
             name="remove"
             class="base-advanced-search-row__search-row-icon" />
         </button>
@@ -406,7 +406,7 @@
         <span
           :id="labelId"
           class="base-advanced-search-row__add-filter-button__text">
-          {{ getI18nTerm(getLangLabel(advancedSearchText.addFilter)) }}
+          {{ assistiveText.addFilter }}
         </span>
       </template>
     </BaseButton>
@@ -635,10 +635,6 @@ export default {
      *     <b>subtext</b>: text shown as second line on the drop down in filters area<br>
      *     <b>availableOptions</b>: text shown with chips options for controlled vocabulary
      *     search<br>
-     *     <b>addFilter</b>: text/label used for add filter icon<br>
-     *     <b>removeFilter</b>: text/label used for remove filter icon<br>
-     *     <b>selectFilterLabel</b>: label (not visible) used for filter chips input field<br>
-     *     <b>searchLabel</b>: label (not visible) used for search input field<br>
      *  <br>
      *  The values of this object might be plain text or a key for an i18n file<br>
      * This prop can be ignored when the 'no-options' slot is used.
@@ -649,10 +645,6 @@ export default {
         title: 'Advanced Search',
         subtext: 'Select a filter',
         availableOptions: 'Available options',
-        addFilter: 'Add filter',
-        removeFilter: 'Remove filter',
-        selectFilterLabel: 'Select filter',
-        searchLabel: 'Search for Entries',
       }),
     },
     /**
@@ -697,6 +689,10 @@ export default {
     /**
      * this prop gives the option to add assistive text for screen readers<br>
      * properties:<br>
+     * <b>addFilter</b>: text/label used for add filter icon<br>
+     * <b>removeFilter</b>: text/label used for remove filter icon<br>
+     * <b>selectFilterLabel</b>: label (not visible) used for filter chips input field<br>
+     * <b>searchLabel</b>: label (not visible) used for search input field<br>
      * <b>selectedOption</b>: text read when a selected option is focused (currently only
      *  working for type chips with autocomplete (=freetext_allowed))
      * **loaderActive**: text that is announced when results are being fetched (prop
@@ -725,6 +721,10 @@ export default {
     assistiveText: {
       type: Object,
       default: () => ({
+        addFilter: 'Add filter',
+        removeFilter: 'Remove filter',
+        selectFilterLabel: 'Select filter',
+        searchLabel: 'Search for Entries',
         selectedOption: '',
         loaderActive: 'loading.',
         autocompleteResultsRetrieved: '{optionsNumber} options found in {collectionsNumber} categories.',

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -22,6 +22,10 @@
         :drop-down-info-texts="dropDownInfoTexts"
         :advanced-search-text="advancedSearchText"
         :assistive-text="{
+          addFilter: assistiveText.addFilter,
+          removeFilter: assistiveText.removeFilter,
+          selectFilterLabel: assistiveText.selectFilterLabel,
+          searchLabel: assistiveText.searchLabel,
           selectedOption: assistiveText.selectedOption,
           loaderActive: assistiveText.autocompleteLoaderActive,
           autocompleteResultsRetrieved: assistiveText.autocompleteResultsRetrieved,
@@ -68,6 +72,10 @@
       :drop-down-info-texts="dropDownInfoTexts"
       :advanced-search-text="advancedSearchText"
       :assistive-text="{
+        addFilter: assistiveText.addFilter,
+        removeFilter: assistiveText.removeFilter,
+        selectFilterLabel: assistiveText.selectFilterLabel,
+        searchLabel: assistiveText.searchLabel,
         selectedOption: assistiveText.selectedOption,
         loaderActive: assistiveText.autocompleteLoaderActive,
         autocompleteResultsRetrieved: assistiveText.autocompleteResultsRetrieved,
@@ -462,10 +470,6 @@ export default {
      *     **subtext**: text shown as second line on the drop-down in filters area for mode `list`.
      *     **availableOptions**: text shown with chips options for controlled vocabulary
      *     search` for mode `list`.
-     *     **addFilter**: text/label used for add filter icon for mode `list`.
-     *     **removeFilter**: text/label used for remove filter icon for mode `list`.
-     *     **selectFilterLabel**: label (not visible) used for filter chips input field for mode `list`.
-     *     **searchLabel**: label (not visible) used for search input field.
      *     **collapsedDateTime**: for mode `form`: set the text for the collapsed filter row which is
      *      displayed for date or time values of ranges when only one field is filled. (e.g. `until 12.12.2023`)
      *     **advancedButtonLabel**: button text displayed for Advanced Search Toggle button for mode `form`.
@@ -479,10 +483,6 @@ export default {
         title: 'Advanced Search',
         subtext: 'Select a filter',
         availableOptions: 'Available options',
-        addFilter: 'Add filter',
-        removeFilter: 'Remove filter',
-        selectFilterLabel: 'Select filter',
-        searchLabel: 'Search for Entries',
         collapsedDateTime: {
           from: 'from',
           until: 'until',
@@ -616,6 +616,10 @@ export default {
      * this prop gives the option to add assistive text for screen readers for
      *  the BaseSearch component.
      * properties:
+     * **addFilter**: text/label used for add filter icon for mode `list`.
+     * **removeFilter**: text/label used for remove filter icon for mode `list`.
+     * **selectFilterLabel**: label (not visible) used for filter chips input field for mode `list`.
+     * **searchLabel**: label (not visible) used for search input field.
      * **selectedOption**: text read when a selected option is focused (currently only
      *  working for type chips with autocomplete (=freetext_allowed))
      * **loaderActive**: text that is announced when autocomplete results are being fetched (prop
@@ -664,6 +668,10 @@ export default {
     assistiveText: {
       type: Object,
       default: () => ({
+        addFilter: 'Add filter',
+        removeFilter: 'Remove filter',
+        selectFilterLabel: 'Select filter',
+        searchLabel: 'Search for Entries',
         selectedOption: '',
         autocompleteLoaderActive: 'loading options.',
         autocompleteResultsRetrieved: '{optionsNumber} options found in {collectionsNumber} categories.',

commit 1b48c2f5f2083738360b986ffcd3ba0ebb263977

fix(BaseAdvancedSearch): provide default values for assistiveText
* caused by commit 63fbf73b moving properties from `advancedSearchText` to `assistiveText`
* since not all properties are required anymore

Refs: #2827

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -15,7 +15,7 @@
       v-model="currentInput"
       :show-pre-input-icon="isMainSearch
         && filter[identifierPropertyName.filter] === defaultFilter[identifierPropertyName.filter]"
-      :label="assistiveText.searchLabel"
+      :label="assistiveText.searchLabel || 'Search for Entries'"
       :type="searchType"
       :selected-chips.sync="selectedOptions"
       :loadable="filter.type === 'text' || filter.type === 'chips'"
@@ -73,7 +73,7 @@
           :use-form-field-styling="false"
           :show-input-border="false"
           :show-label="false"
-          :label="assistiveText.selectFilterLabel"
+          :label="assistiveText.selectFilterLabel || 'Select filter'"
           :language="language"
           :drop-down-list-id="'filter-options-' + internalRowId"
           :identifier-property-name="identifierPropertyName.filter"
@@ -120,7 +120,7 @@
           @focusin.stop.prevent
           @click.stop.prevent="removeFilter">
           <BaseIcon
-            :title="assistiveText.removeFilter"
+            :title="assistiveText.removeFilter || 'Remove filter'"
             name="remove"
             class="base-advanced-search-row__search-row-icon" />
         </button>
@@ -406,7 +406,7 @@
         <span
           :id="labelId"
           class="base-advanced-search-row__add-filter-button__text">
-          {{ assistiveText.addFilter }}
+          {{ assistiveText.addFilter || 'Add filter'}}
         </span>
       </template>
     </BaseButton>