a1290fdfc26c06eb1d9da2395f32b81608f0a486
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2024-12-12 11:41:39 +0100

    prepare(BaseHlsVideo): load hls.js library asynchronously
    * add loading indicator
    * restructure code sections

--- src/components/BaseHlsVideo/BaseHlsVideo.vue
+++ src/components/BaseHlsVideo/BaseHlsVideo.vue
@@ -1,43 +1,16 @@
-<template>
-  <div
-    class="base-media-video">
-    <button
-      v-if="playButton"
-      ref="playButton"
-      :title="`${getI18nTerm(buttonTitle)} Video`"
-      class="base-media-video__control"
-      @keydown.enter.space="play"
-      @click="play">
-      <BaseIcon
-        name="play"
-        class="base-media-video__icon" />
-    </button>
-
-    <!-- tabindex is needed for swiper to handle focus after using tab key -->
-    <video
-      ref="videoPlayer"
-      :style="displaySize"
-      :poster="mediaPosterUrl"
-      :tabindex="playButton ? '-1' : '0'"
-      class="base-media-video__video">
-      Your browser does not support the video tag.
-    </video>
-  </div>
-</template>
-
 <script>
-import Hls from 'hls.js/dist/hls.light.js';
+import { ref, defineAsyncComponent } from 'vue';
 import { useI18n } from '@/composables/useI18n.js';
-import { defineAsyncComponent } from 'vue';
 
 export default {
   name: 'BaseHlsVideo',
   components: {
     BaseIcon: defineAsyncComponent(() => import('@/components/BaseIcon/BaseIcon.vue')),
+    BaseLoader: defineAsyncComponent(() => import('@/components/BaseLoader/BaseLoader.vue')),
   },
   props: {
     /**
-     * url of the medium to be displayed
+     * url of the media to be displayed
      */
     mediaUrl: {
       type: String,
@@ -74,107 +47,160 @@ export default {
     },
     /**
      * define startLevel (size) of hls-video
+     * Note: The default value of -1 indicates automatic start level selection.
      */
     hlsStartLevel: {
       type: Number,
-      default: undefined,
+      default: -1,
     },
   },
   setup() {
     const { getI18nTerm } = useI18n();
+    const isLoading = ref(false);
+    const hls = ref(() => {});
+    const playButton = ref(true);
+    const video = ref();
     return {
       getI18nTerm,
-    };
-  },
-  data() {
-    return {
-      hsl: () => {},
-      video: null,
-      playButton: true,
+      isLoading,
+      hls,
+      playButton,
+      video,
     };
   },
   mounted() {
-    this.video = this.$refs.videoPlayer;
-
-    if (this.autoplay) {
-      this.play();
-    }
+    // play video if autoplay is enabled
+    if (this.autoplay) this.play();
   },
   unmounted() {
     // destroy hls object to stop buffering and save bandwidth
-    if (this.hls) {
-      this.hls.destroy();
-    }
+    if (this.hls.value) this.hls.value.destroy();
   },
   methods: {
     /**
      * init video
      */
-    init() {
-      if (this.video) {
-        if (Hls.isSupported()) {
-          if (!this.hls) {
-            this.hls = new Hls({
-              startLevel: this.hlsStartLevel,
-            });
-            this.hls.loadSource(this.mediaUrl);
-            this.hls.attachMedia(this.video);
-            this.hls.on(Hls.Events.MANIFEST_PARSED, () => {
-              this.play();
-              this.playButton = false;
-              this.video.controls = true;
-              this.video.focus();
-            });
-          }
-        } else if (this.video.canPlayType('application/vnd.apple.mpegurl')
-            || this.video.canPlayType('application/vnd.apple.mpegurl') === 'maybe') {
-          this.hls = false;
-          this.video.src = this.mediaUrl;
-          this.video.addEventListener('loadedmetadata', () => {
-            this.video.play();
+    async init() {
+      // check if the video element exists, otherwise do nothing
+      if (!this.video) return;
+      // set loading state
+      this.isLoading = true;
+      // import hls.js library
+      const HlsLight = await import('hls.js/dist/hls.light.js');
+      // set loading state
+      this.isLoading = false;
+      // map hls.js library for internal use
+      const Hls = HlsLight.default;
+      // check if hls is supported by the browser
+      if (Hls.isSupported()) {
+        // if hls is not initialised yet
+        if (!this.hls.value) {
+          // initialise the hls.js library
+          this.hls.value = new Hls({
+            startLevel: this.hlsStartLevel,
+          });
+          // add the video source
+          this.hls.value.loadSource(this.mediaUrl);
+          // attach media to the video element
+          this.hls.value.attachMedia(this.video);
+          // handle event when the video has finished loading
+          this.hls.value.on(Hls.Events.MANIFEST_PARSED, () => {
+            this.play();
+            this.playButton = false;
+            this.video.controls = true;
             this.video.focus();
           });
         }
-
-        this.video.addEventListener('pause', () => {
-          this.playButton = true;
+      // else try to use the native video element
+      } else if (this.video.canPlayType('application/vnd.apple.mpegurl')
+          || this.video.canPlayType('application/vnd.apple.mpegurl') === 'maybe') {
+        // set hls state
+        this.hls.value = false;
+        // add media to the video element
+        this.video.src = this.mediaUrl;
+        // handle event when the video has finished loading
+        this.video.addEventListener('loadedmetadata', () => {
+          this.video.play();
+          this.video.focus();
+          this.video.controls = true;
         });
-
-        this.video.addEventListener('play', () => {
-          this.playButton = false;
-        });
+      } else {
+        console.warn('Your browser does not support the video tag.');
+        return;
       }
+      // handle event to pause the video
+      this.video.addEventListener('pause', () => {
+        this.playButton = true;
+      });
+      // handle event to play the video
+      this.video.addEventListener('play', () => {
+        this.playButton = false;
+      });
     },
     /**
      * play video
      */
     play() {
-      if (this.video) {
-        if (this.hls || this.video.src) {
-          this.video.play();
-          return;
-        }
-
-        this.init();
+      if (!this.video) return;
+      // check if the video player is already initialised and play the video
+      if (this.hls.value || this.video.src) {
+        this.video.play();
+        return;
       }
+      // otherwise initialise the video player
+      this.init();
     },
     /**
      * pause video
      */
     pause() {
-      if (this.video) {
-        this.video.pause();
-      }
+      if (!this.video) return;
+      this.video.pause();
     },
   },
 };
 </script>
+
+<template>
+  <div
+    class="base-media-video">
+    <div
+      v-if="isLoading"
+      class="base-media-video__loader">
+      <BaseLoader
+        class="base-media-video__loader__inner" />
+    </div>
+
+    <button
+      v-if="playButton && !isLoading"
+      ref="playButton"
+      :title="`${getI18nTerm(buttonTitle)} Video`"
+      class="base-media-video__control"
+      @keydown.enter.space="play"
+      @click="play">
+      <BaseIcon
+        name="play"
+        class="base-media-video__icon" />
+    </button>
+
+    <!-- tabindex is needed for swiper to handle focus after using tab key -->
+    <video
+      ref="video"
+      :style="displaySize"
+      :poster="mediaPosterUrl"
+      :tabindex="playButton ? '-1' : '0'"
+      class="base-media-video__video">
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
 
 <style lang="scss" scoped>
   @use "@/styles/variables" as *;
 
   .base-media-video {
     position: relative;
+    min-height: 50px;
 
     &__control {
       position: absolute;
@@ -201,11 +227,26 @@ export default {
     &__video {
       max-height: 100%;
       max-width: 100%;
+      vertical-align: middle;
     }
 
     &__icon {
       height: 100%;
       width: 100%;
+    }
+
+    &__loader {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &__inner {
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
     }
   }
 </style>
