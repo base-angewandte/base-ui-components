ef4f5713a2257a182b394b23e580e09b477e811f
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2024-12-17 12:49:26 +0100

    prepare(BaseMap): refactor to use Vue3 composition API
    * add `IntersectionObserver` to composable  useElementObserver
    
    Refs: #2892

--- src/composables/useElementObserver.js
+++ src/composables/useElementObserver.js
@@ -1,14 +1,20 @@
 import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
 
 /**
+ * @typedef {Object} IntersectionObserverOptions
+ * @property {boolean} triggerCallbackWhenInViewport - fire callback when target is in viewport only
+ * @property {boolean} stopObservingWhenInViewport - disconnect the observer when target is in viewport
+ */
+
+/**
  * use this composable to add a resize or mutation observer to a component
  * @param {string} type - specify the type of observer
- *  @values 'resize', 'mutation'
+ *  @values 'resize', 'mutation', 'intersection'
  * @param {Ref<UnwrapRef<HTMLElement>>} target - the element to observe changes on
  * @param {function} callback - the function to be executed when the callback
  *  of the observer is triggered
- * @param {object} [options={}] - options specific for that type of observer
- * @returns {{observer: Ref<UnwrapRef<null|MutationObserver|ResizeObserver>>}}
+ * @param {object|IntersectionObserverOptions} [options={}] - options specific for that type of observer
+ * @returns {{observer: Ref<UnwrapRef<null|MutationObserver|ResizeObserver|IntersectionObserver>>}}
  */
 export function useElementObserver({ type, target, callback, options = {} }) {
   // create a reference to the provided target element, also check if value can
@@ -31,6 +37,18 @@ export function useElementObserver({ type, target, callback, options = {} }) {
     } else if (type === 'mutation') {
       observer.value = new MutationObserver(() => {
         callback();
+      });
+      // or create a new intersection observer
+    } else if (type === 'intersection') {
+      observer.value = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          // if option `triggerCallBackWhenInViewport` is set and not in viewport return immediately
+          if (options?.triggerCallbackWhenInViewport && !entry.isIntersecting) return;
+          // if the option `stopObservingWhenInViewport` is set, disconnect the observer
+          if (options?.stopObservingWhenInViewport) observer.value.disconnect();
+          // execute the callback function
+          callback();
+        });
       });
     } else {
       console.warn(`type '${type}' can currently not be handled in useElementObserver.`);
