base-angewandte/base-ui-components

commit d0509e31072b39a54cd4ad9992a4a008c7cae25c

fix(BaseButton)!: fix styleguide slot `text` example and auto add margin if text (or slot) and icon are present
BREAKING-CHANGE: if prop `text` or slot `text` and icon are present spacing is added

src/components/BaseButton/BaseButton.vue
@@ -204,7 +204,8 @@ export default {
       :class="['base-button-icon',
                'base-button-icon-' + iconSize,
                { 'base-button-icon-colored': iconColored },
-               { 'base-button-icon-hide': hideIcon }]" />
+               { 'base-button-icon-hide': hideIcon },
+               { 'base-button-icon-margin': buttonHasTextElement }]" />
 
     <!-- @slot create custom content (e.g. icon) left of text -->
     <slot name="left-of-text" />
@@ -405,8 +406,8 @@ export default {
     }
 
     &.base-button-icon-left {
-      .base-button-icon + .base-button-text {
-        margin-left: $spacing;
+      .base-button-icon-margin {
+        margin-right: $spacing;
       }
     }
 
@@ -414,8 +415,8 @@ export default {
       .base-button-icon {
         order: 1;
 
-        & + .base-button-text {
-          margin-right: $spacing;
+        &.base-button-icon-margin {
+          margin-left: $spacing;
         }
       }
     }

commit da2f7e9d3a148ea2efd7eb006360c3edd5fa942b

prepare(BaseButton): upgrade to Vue 3: fix margin wrong for button type 'circle'
* bug introduced in commit d0509e31

src/components/BaseButton/BaseButton.vue
@@ -162,7 +162,7 @@ export default {
 
     // compute if internal id should be set for aria-attribute
     // (no need if no text or slot is not used)
-    const buttonHasTextElement = computed(() => !!props.text || slotHasContent.value);
+    const buttonHasTextElement = computed(() => (!!props.text  || slotHasContent.value) && props.buttonStyle !== 'circle');
 
     return {
       internalId,