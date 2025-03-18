296900874f2bed80c249b0969d767157b3cf547a
Bernhard Saltuari <bernhard.saltuari@uni-ak.ac.at>
2025-03-18 11:13:02 +0100

    prepare(BaseExpandBox): upgrade to Vue 3: switch to BEM css notation

--- src/components/BaseExpandRow/BaseExpandRow.vue
+++ src/components/BaseExpandRow/BaseExpandRow.vue
@@ -3,14 +3,14 @@
     :class="['base-expand-row',
              { 'base-expand-row--expandable': isExpandable },
              { 'base-expand-row--expanded': isExpandedInternal }]">
-    <div class="base-expand-row-header">
+    <div class="base-expand-row__header">
       <BaseCheckmark
         v-if="isSelectable"
         :key="internalId + 'checkmark'"
         title="checkbox"
         mark-style="checkbox"
-        class="base-expand-row-checkbox"
         :model-value="isSelected"
+        class="base-expand-row__header__checkbox"
         @update:model-value="checkboxClicked" />
       <component
         :is="isExpandable ? 'button' : 'div'"
@@ -18,11 +18,11 @@
         :aria-controls="isExpandable ? 'base-expand-row-panel-' + internalId : null"
         :aria-expanded="isExpandable ? isExpandedInternal.toString() : null"
         :type="isExpandable ? 'button' : null"
-        :class="['base-expand-row-button', { selectable: isSelectable }]"
+        :class="['base-expand-row__header__button', { selectable: isSelectable }]"
         @click="isExpandable ? clicked() : null">
         <div
           v-if="icon || hasIconSlot"
-          class="base-expand-row-icon">
+          class="base-expand-row__header__icon">
           <!-- @slot slot to inject icon/image left side before label -->
           <slot name="icon">
             <BaseIcon
@@ -31,7 +31,7 @@
         </div>
 
         <div
-          class="base-expand-row-title-subtitle-wrapper">
+          class="base-expand-row__header__wrapper">
           <div
             v-if="!title && !subtitle"
             class="hide">
@@ -39,19 +39,19 @@
           </div>
           <div
             v-if="title"
-            class="base-expand-row-title base-text-fade-out">
+            class="base-expand-row__header__title base-text-fade-out">
             {{ title }}
           </div>
           <div
             v-if="subtitle"
-            class="base-expand-row-subtitle base-text-fade-out">
+            class="base-expand-row__header__subtitle base-text-fade-out">
             {{ subtitle }}
           </div>
         </div>
         <BaseIcon
           v-if="isExpandable"
           name="drop-down"
-          class="base-expand-row-collapse-icon" />
+          class="base-expand-row__header__collapse-icon" />
       </component>
     </div>
     <div
@@ -59,7 +59,7 @@
       :aria-labelledby="'base-expand-row-' + internalId"
       :aria-hidden="!isExpandedInternal ? 'true' : 'false'"
       :role="'region'"
-      :class="['base-expand-row-body', { 'base-expand-row-body-bg': bodyHasBackground }]">
+      :class="['base-expand-row__body', { 'base-expand-row__body--bg': bodyHasBackground }]">
       <!-- @slot slot for expanded content -->
       <slot />
     </div>
@@ -249,7 +249,7 @@ export default {
     margin-bottom: $border-width;
     background-color: $background-color;
 
-    .base-expand-row-header {
+    .base-expand-row__header {
       display: flex;
       justify-content: flex-start;
       align-items: center;
@@ -257,12 +257,12 @@ export default {
       box-shadow: $box-shadow-reg;
       transition: $box-transition;
 
-      .base-expand-row-checkbox {
+      .base-expand-row__header__checkbox {
         padding: 0 $spacing;
         width: $spacing * 3;
       }
 
-      .base-expand-row-button {
+      .base-expand-row__header__button {
         height: $row-height-large;
         padding: 0 $spacing;
         width: 100%;
@@ -270,26 +270,26 @@ export default {
         justify-content: flex-start;
         align-items: center;
 
-        .base-expand-row-title-subtitle-wrapper {
+        .base-expand-row__header__wrapper {
           flex: auto;
           margin-right: $spacing;
           overflow: hidden;
-
-          .base-expand-row-title {
-            color: $font-color;
-            text-align: left;
-            white-space: nowrap;
-          }
-
-          .base-expand-row-subtitle {
-            font-size: $font-size-small;
-            color: $font-color-second;
-            text-align: left;
-            white-space: nowrap;
-          }
         }
 
-        .base-expand-row-icon {
+        .base-expand-row__header__title {
+          color: $font-color;
+          text-align: left;
+          white-space: nowrap;
+        }
+
+        .base-expand-row__header__subtitle {
+          font-size: $font-size-small;
+          color: $font-color-second;
+          text-align: left;
+          white-space: nowrap;
+        }
+
+        .base-expand-row__header__icon {
           height: $icon-large;
           width: $icon-large;
           margin-right: $spacing;
@@ -302,7 +302,7 @@ export default {
           }
         }
 
-        .base-expand-row-collapse-icon {
+        .base-expand-row__header__collapse-icon {
           transition: $drop-down-arrow-animation;
           height: $icon-small;
           width: $icon-small;
@@ -312,7 +312,7 @@ export default {
 
         &.selectable {
           // if row is selectable, we must account for the width of the check box.
-          // therefore, decrease 100% width by the width of .base-expand-row-checkbox
+          // therefore, decrease 100% width by the width of .base-expand-row__header__checkbox
           width: calc(100% - #{$spacing} * 3);
           padding: 0 $spacing 0 0;
         }
@@ -323,24 +323,23 @@ export default {
       }
     }
 
-    .base-expand-row-body {
+    .base-expand-row__body {
       padding: $spacing;
 
-      &.base-expand-row-body-bg {
+      &.base-expand-row__body--bg {
         margin-top: $border-width;
         background-color: $box-color;
       }
     }
 
     &.base-expand-row--expandable {
-
-      .base-expand-row-button {
+      .base-expand-row__header__button {
 
         &:focus,
         &:hover {
           cursor: pointer;
 
-          .base-expand-row-collapse-icon {
+          .base-expand-row__header__collapse-icon {
             color: $app-color;
           }
         }
@@ -348,11 +347,10 @@ export default {
     }
 
     &.base-expand-row--expanded {
-
-      .base-expand-row-header {
-        .base-expand-row-button {
-          .base-expand-row-title-subtitle-wrapper {
-            .base-expand-row-title {
+      .base-expand-row__header {
+        .base-expand-row__header__button {
+          .base-expand-row__header__wrapper {
+            .base-expand-row__header__title {
               color: $font-color;
               font-weight: bold;
             }
@@ -360,16 +358,16 @@ export default {
         }
       }
 
-      .base-expand-row-body {
+      .base-expand-row__body {
         display: block;
       }
 
-      .base-expand-row-collapse-icon {
+      .base-expand-row__header__collapse-icon {
         transform: rotate(180deg);
       }
     }
 
-    .base-expand-row-body {
+    .base-expand-row__body {
       display: none;
     }
   }
