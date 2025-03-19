base-angewandte/base-ui-components

commit a09ec18f1cd3bce0061cdda1f179297a2b58ad14

prepare(BaseAdvancedSearch): upgrade to Vue 3: fix issues with date input sync and keydown event on input and remove button

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -60,9 +60,9 @@
       @keydown.stop="handleKeyDownEvent"
       @keydown.up.down.right.left="navigateDropDown"
       @keydown.tab="handleDropDownOnTabKey"
-      @keydown.enter.capture="selectOptionOnKeyEnter"
+      @keydown.enter.stop.prevent="selectOptionOnKeyEnter"
       @keydown.esc="isActive = false"
-      @value-validated="searchType === 'date' ? handleDateInput : null">
+      @value-validated="handleDateInput">
       <!-- FIRST COLUMN OF SEARCH FIELD (FILTERS) -->
       <template #[filterSlotName]>
         <BaseChipsInputField
@@ -120,8 +120,9 @@
             || filterHasValues || (filter.type === 'text' && currentInput)"
           :class="['base-advanced-search-row__icon-button',
                    { 'base-advanced-search-row__icon-button__date': filter.type.includes('date') }]"
-          @keydown.tab="onTab"
           @focusin.stop.prevent
+          @keydown.tab="onTab"
+          @keydown.stop.prevent="removeFilter"
           @click.stop.prevent="removeFilter">
           <BaseIcon
             :title="assistiveText.removeFilter || 'Remove filter'"
@@ -154,6 +155,7 @@
           class="base-advanced-search-row__drop-down-body"
           @touchstart.stop=""
           @click.stop="">
+          <!-- FILTER AREA -->
           <template
             v-if="mode === 'list'"
             #before-list>
@@ -1642,7 +1644,7 @@ export default {
       } else if (this.filter.type === 'text') {
         const newTextArray = [].concat(this.currentInput);
         if (JSON.stringify(this.filter.filter_values) !== JSON.stringify(newTextArray)) {
-          this.filter.filter_values = [].concat(this.currentInput);
+          this.filter.filter_values = newTextArray;
           this.isActive = false;
         } else {
           this.isActive = !this.isActive;

commit 1273a455565ac73bf015845efffdd5fb38314e54

prepare(BaseAdvancedSearch): upgrade to Vue 3: fix remove filter triggered on every keydown
* bug introduced in commit a09ec18f
* instead of just enter, every key was used for remove

Refs: #2905

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -122,7 +122,7 @@
                    { 'base-advanced-search-row__icon-button__date': filter.type.includes('date') }]"
           @focusin.stop.prevent
           @keydown.tab="onTab"
-          @keydown.stop.prevent="removeFilter"
+          @keydown.enter.stop.prevent="removeFilter"
           @click.stop.prevent="removeFilter">
           <BaseIcon
             :title="assistiveText.removeFilter || 'Remove filter'"