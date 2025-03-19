base-angewandte/base-ui-components

commit 0cbefb69fa130c90a1589204f1215c18fe434445

prepare(BaseForm): upgrade to Vue 3: more minor fixes
* e.g. update to BaseForm modelValue prop
* stop click and keydown events from triggering drop down

src/components/BaseAdvancedSearch/BaseCollapsedFilterRow.vue
@@ -109,7 +109,8 @@
     <button
       :title="assistiveText.removeFiltersLabel"
       class="base-collapsed-filter-row__remove"
-      @click="removeFilters">
+      @keydown.stop="removeFilters"
+      @click.stop="removeFilters">
       <BaseIcon
         name="remove"
         class="base-collapsed-filter-row__remove-icon" />

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -118,10 +118,10 @@
       <template #below>
         <BaseForm
           v-if="mode === 'form' && formOpen"
-          :id="`${internalId}-form`"
           v-bind="amendedFormProps"
+          :model-value="formFilterValuesInt"
+          :form-id="`${internalId}-form`"
           :form-field-json="formFilterList"
-          :value-list="formFilterValuesInt"
           :label-property-name="labelPropertyName.formInputs"
           :identifier-property-name="identifierPropertyName.formInputs"
           :class="['base-advanced-search__search-form',
@@ -129,6 +129,7 @@
           @input-complete="updateFormFilters"
           @fetch-autocomplete="fetchFormAutocomplete"
           @form-mounted="formIsMounted"
+          @keydown.stop
           @click.stop>
           <template #label-addition="{ fieldName, groupNames }">
             <!-- @slot all [BaseForm](BaseForm.html#slots) slots are available with the prefix 'form-'. For a more detailed description and demonstration refer to [BaseForm](BaseForm.html#slots).
@@ -248,6 +249,8 @@
               booleanFilterLabel: assistiveText.collapsedBooleanFilterValue,
               optionToRemoveSelected: assistiveText.collapsedOptionToRemoveSelected,
             }"
+            @click.stop
+            @keydown.stop
             @remove-all="removeAllFilters" />
         </div>
       </template>
@@ -1128,7 +1131,7 @@ export default {
     formFilterValuesInt: {
       handler(val) {
         // get filters without default first to ensure comparability
-        const filtersWithoutDefault = { ...this.formFilterValues };
+        const filtersWithoutDefault = JSON.parse(JSON.stringify(this.formFilterValues));
         delete filtersWithoutDefault.default;
         // check if val is actually different from prop value
         if (JSON.stringify(val) !== JSON.stringify(filtersWithoutDefault)) {

commit aa5e7c91590e1f0f0146f824fc9593241a6b2895

prepare(BaseAdvancedSearch): upgrade to Vue 3: fix remove collapsed row triggered on every keydown
* bug introduced in commit 0cbefb69
* instead of just enter, every key was used for remove

Refs: #2905

src/components/BaseAdvancedSearch/BaseCollapsedFilterRow.vue
@@ -107,7 +107,7 @@
     <button
       :title="assistiveText.removeFiltersLabel"
       class="base-collapsed-filter-row__remove"
-      @keydown.stop="removeFilters"
+      @keydown.enter.stop="removeFilters"
       @click.stop="removeFilters">
       <BaseIcon
         name="remove"