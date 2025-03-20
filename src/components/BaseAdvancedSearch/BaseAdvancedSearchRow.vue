base-angewandte/base-ui-components

commit d0509e31072b39a54cd4ad9992a4a008c7cae25c

fix(BaseButton)!: fix styleguide slot `text` example and auto add margin if text (or slot) and icon are present
BREAKING-CHANGE: if prop `text` or slot `text` and icon are present spacing is added

src/components/BaseButton/BaseButton.vue
@@ -204,7 +204,8 @@ export default {
       :class="['base-button-icon',
                'base-button-icon-' + iconSize,
                { 'base-button-icon-colored': iconColored },
-               { 'base-button-icon-hide': hideIcon }]" />
+               { 'base-button-icon-hide': hideIcon },
+               { 'base-button-icon-margin': buttonHasTextElement }]" />
 
     <!-- @slot create custom content (e.g. icon) left of text -->
     <slot name="left-of-text" />
@@ -405,8 +406,8 @@ export default {
     }
 
     &.base-button-icon-left {
-      .base-button-icon + .base-button-text {
-        margin-left: $spacing;
+      .base-button-icon-margin {
+        margin-right: $spacing;
       }
     }
 
@@ -414,8 +415,8 @@ export default {
       .base-button-icon {
         order: 1;
 
-        & + .base-button-text {
-          margin-right: $spacing;
+        &.base-button-icon-margin {
+          margin-left: $spacing;
         }
       }
     }

commit 6581ef44c9bbe4465669b7f0a792aa251cab01ff

prepare(BaseAdvancedSearch): fix styling issues caused by previous commits
* caused by commit d0509e31

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -2037,10 +2037,6 @@ export default {
         height: $icon-medium;
         width: $icon-medium;
       }
-
-      .base-advanced-search-row__icon-button__text {
-        display: none;
-      }
     }
 
     .base-advanced-search-row__drop-down-body {
@@ -2208,11 +2204,17 @@ export default {
   width: $row-height-large;
 
   .base-advanced-search-row__add-filter-button__text {
-    display: none;
+    position: absolute;
+    top: auto;
+    left: -9999px;
   }
 
   &__shadow {
     box-shadow: $box-shadow-reg;
+  }
+
+  &.base-button:deep(.base-button__icon.base-button__icon--margin) {
+    margin-left: 0;
   }
 }
 
@@ -2236,8 +2238,10 @@ export default {
       width: 100%;
 
       .base-advanced-search-row__add-filter-button__text {
+        position: relative;
+        top: auto;
+        left: unset;
         display: inline;
-        margin-right: $spacing;
       }
     }
 

src/components/BaseButton/BaseButton.vue
@@ -419,6 +419,7 @@ export default {
       .base-button__icon {
         order: 1;
 
+        // this class is used in BaseAdvancedSearchRow
         &.base-button__icon--margin {
           margin-left: $spacing;
         }