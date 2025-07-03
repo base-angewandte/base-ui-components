base-angewandte/base-ui-components

commit d97cd4b6b8d18c4d4db8fa53f4efff6c5a2dc874

prepare(useEventListener, useWindowResize, BaseLink, BaseTooltipBox): target 'window' not defined on setup in SSR context
Refs: #2963

src/components/BaseTooltipBox/BaseTooltipBox.vue
@@ -204,7 +204,8 @@ function escEventHandler(e) {
 }
 
 useEventListener({
-  target: window,
+  // need to pass a string since window not defined in setup for SSR
+  target: 'window',
   event: 'keydown',
   callback: escEventHandler,
 });

src/components/BaseLink/BaseLink.vue
@@ -2,7 +2,6 @@
 import { defineAsyncComponent, ref, getCurrentInstance, computed, useSlots } from 'vue';
 import { useWindowResize } from '@/composables/useWindowResize.js';
 import { useEventListener } from '@/composables/useEventListener.js';
-import { useDebounce } from '@/composables/useDebounce.js';
 import { useId } from '@/composables/useId.js';
 import { useHasSlotContent } from '@/composables/useHasSlotContent.js';
 import cleanDomNodes from '@/directives/cleanDomNodes.js';
@@ -225,10 +224,6 @@ export default {
      */
     const showTooltip = ref(false);
 
-    // get the debounce function to put a timeout
-    // on scroll and resize event listeners
-    const { debounce } = useDebounce();
-
     /**
      * close an open tooltip
      */
@@ -240,23 +235,24 @@ export default {
      * intercept scroll/resize event and close the tooltip
      */
     function scrollResizeHandler() {
-      debounce(() => {
-        if (showTooltip.value) {
-          closeTooltip();
-        }
-      }, 100);
+      if (showTooltip.value) {
+        closeTooltip();
+      }
     }
 
     // use the window resize event listener
     useWindowResize({
       callback: scrollResizeHandler,
+      setDebounce: 100,
     });
     // and additionally create a scroll event listener to also
     // close the tooltip when scrolling is happening on the page
     useEventListener({
-      target: window,
+      // need to pass a string since window not defined in setup for SSR
+      target: 'window',
       event: 'scroll',
       callback: scrollResizeHandler,
+      setDebounce: 100,
     });
 
     // access slots to check if it is filled later

src/composables/useEventListener.js
@@ -3,8 +3,9 @@ import { useDebounce } from '@/composables/useDebounce.js';
 
 /**
  * put an even listener on an HTML element
- * @param {HTMLElement|Ref<HTMLElement>|Window} target - the html element the listener should be attached to - could be a native
- *  element (e.g. 'body') or a reference to an element (or a vue component)
+ * @param {HTMLElement|Ref<HTMLElement>|string} target - the html element the listener should be attached to - could be a native
+ *  element (e.g. 'body') or a reference to an element (or a vue component) - if window or document should be used just
+ *  provide the appropriate string (needed since window or document are not available in setup on server side rendering)
  * @param {string} event - the name of the event that should be listened to
  * @param {function} callback - the function to be executed when the event is triggered
  * @param {boolean|Object} [options={}] - the event listener options
@@ -44,27 +45,58 @@ export function useEventListener({
 
   // on mounted the listener is attached
   onMounted(() => {
-    if (callOnMounted) {
-      callback();
+    // check if a plain string was provided as target which we need to do for SSR, since we can not
+    // pass window or document element in setup
+    if (typeof listenerContainer.value === 'string') {
+      if (callOnMounted) {
+        callback();
+      }
+      if (listenerContainer.value === 'window') {
+        window.addEventListener(event, listenerCallback.value, options);
+      } else if (listenerContainer.value === 'document') {
+        document.addEventListener(event, listenerCallback.value, options);
+      } else {
+        console.warn(`${listenerContainer.value} is not a valid EventListener element`);
+      }
+      return;
     }
+    // else treat the listenerContainer as ref to an HTML element
+    // check if the element is already present
     if (listenerContainer.value) {
+      if (callOnMounted) {
+        callback();
+      }
       listenerContainer.value.addEventListener(event, listenerCallback.value, options);
+      // if not - set up a watcher that will set up the listener as soon as the listenerContainer
+      // has a value
     } else {
+      console.log('container not initialized yet');
       watch(listenerContainer, (container) => {
+        console.log('setting up now', container);
         if (container) {
           if (callOnMounted) {
             callback();
           }
           container.addEventListener(event, listenerCallback.value, options);
         }
-      });
+      }, { once: true });
     }
   });
 
   // and on before unmount it is removed again
   onBeforeUnmount(() => {
+    // check if a plain string was provided as target which we need to do for SSR, since we can not
+    // pass window or document element in setup
+    if (typeof listenerContainer.value === 'string') {
+      if (listenerContainer.value === 'window') {
+        window.removeEventListener(event, listenerCallback.value, options);
+      } else if (listenerContainer.value === 'document') {
+        document.removeEventListener(event, listenerCallback.value, options);
+      }
+      return;
+    }
     if (listenerContainer.value) {
-      listenerContainer.value.removeEventListener(event, listenerCallback.value);
+      listenerContainer.value.removeEventListener(event, listenerCallback.value, options);
     }
   });
 }

src/composables/useWindowResize.js
@@ -42,12 +42,13 @@ export function useWindowResize({
    * execute the function provided to the composable
    */
   function resize() {
-    windowInnerWidth.value = window.innerWidth;
+    // safeguard against window not being defined yet
+    windowInnerWidth.value = window?.innerWidth || 0;
     callback();
   }
 
   useEventListener({
-    target: window,
+    target: 'window',
     event: 'resize',
     callback: resize,
     setDebounce,

commit f508ee40980fa3b32d852f72e88c5064840a7ab1

prepare(BaseTooltipBox): initial window with not calculated correctly after refactor
* caused by commit d97cd4b6

Ref: #2963

src/components/BaseTooltipBox/BaseTooltipBox.vue
@@ -181,6 +181,7 @@ const isActive = ref(false);
 const { isMobile } = useWindowResize({
   callback: calcPosition,
   mobileMaxSize: props.mobileSize,
+  callOnMounted: true,
 });
 
 /** CLICK OUTSIDE HANDLING */