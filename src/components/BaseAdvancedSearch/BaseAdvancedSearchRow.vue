base-angewandte/base-ui-components

commit a4564ce63290a4158bf7907d4b5ccb8f62fd5ea4

prepare(BaseAdvancedSearch): upgrade to Vue 3: fixing refactoring problems for mode `list`
* issues with filterValues syncing
* observer loop error
* focus on adding new filter row

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -800,7 +800,62 @@ export default {
      * the currently selected filter
      * @type {Filter}
      */
-    const filter = ref(null);
+    const filter = ref({
+      ...props.defaultFilter,
+      filter_values: props.defaultFilter.values || setFilterValues(props.defaultFilter),
+    });
+
+    /**
+     * function to set the correct values for filter.filter_values attribute
+     *
+     * @param {Filter} newFilter - the newly selected filter
+     * @param {Filter} previousFilter - the previously set filter
+
+     * @returns {?string|Array|Object} the correct value type for the filter type
+     */
+    function setFilterValues(newFilter, previousFilter = {}) {
+      const previousFilterValues = previousFilter.filter_values;
+      const { type } = newFilter;
+      const freetextAllowed = newFilter.freetext_allowed;
+      if (type === 'date') {
+        // map the date from daterange to date if necessary
+        return previousFilter?.type.includes('date')
+        && previousFilterValues ? previousFilterValues.date_from || previousFilterValues.date_to : '';
+      }
+      if (type === 'daterange') {
+        // check if it can be mapped from date to daterange
+        if (previousFilter.type.includes('date')) {
+          return {
+            date_from: previousFilterValues ? previousFilterValues.date_from || previousFilterValues || '' : '',
+            date_to: previousFilterValues?.to ? previousFilterValues.to : '',
+          };
+        }
+        // else just return empty object
+        return {
+          date_from: '',
+          date_to: '',
+        };
+      }
+      // check if both are autocomplete chips filters
+      if (type === 'chips' && freetextAllowed && previousFilter.type === 'chips' && previousFilter.freetext_allowed) {
+        // check if new filter is a superset filter of the previous filter (=previous
+        // filter id is included in current filter 'subsets' property
+        if (newFilter.subsets?.includes(previousFilter[props.identifierPropertyName.filter])) {
+          return previousFilterValues;
+        }
+        // if both are chips with freetext keep options without id (=not specific entries)
+        return previousFilterValues?.length ? previousFilterValues
+          .filter(value => !value[props.identifierPropertyName.autocompleteOption]) : [];
+      }
+      // check if previous filter was text and new filter is autocomplete chips
+      if (previousFilter.type === 'text' && previousFilterValues?.length
+        && !!previousFilterValues[0] && type === 'chips' && freetextAllowed) {
+        return [{
+          [this.labelPropertyName.autocompleteOption]: previousFilterValues[0],
+        }];
+      }
+      return [];
+    }
 
     /** INTERNAL ID */
     const internalId = useId();
@@ -854,7 +909,12 @@ export default {
         // SET CORRECT SLOT FOR SELECTED FILTER DISPLAY
         // get the width of the element
         const searchRowElementWidth = advancedSearchRow.value.clientWidth;
-        filterSlotName.value = searchRowElementWidth >= 500 ? 'pre-input-field' : 'input-field-addition-before';
+        // need to set time out and put this assignment at the end of all observer callback executions
+        // so it is not interfering with the still unfinished observer callback (making changes to DOM
+        // while observer callback is still ongoing, causing a loop)
+        setTimeout(() => {
+          filterSlotName.value = searchRowElementWidth >= 500 ? 'pre-input-field' : 'input-field-addition-before';
+        }, 0);
       }
     }
 
@@ -873,7 +933,7 @@ export default {
     /**
      * store the current filter type to recognize when it changes and only take
      * action e.g. on focusing input field, after it was rendered
-     * @type {string}
+     * @type {Ref<UnwrapRef<string>>}
      */
     const currentFilterType = ref(props.appliedFilter ? props.appliedFilter.type : props.defaultFilter.type);
 
@@ -955,6 +1015,7 @@ export default {
       baseSearch,
       // filter data
       filter,
+      setFilterValues,
       // internal id
       internalRowId,
       // drop down navigation
@@ -1229,24 +1290,10 @@ export default {
            * @event update:applied-filter
            * @property {Filter} val - the new currently applied filter
            */
-          this.$emit('update:applied-filter', { ...val });
+          this.$emit('update:applied-filter', JSON.parse(JSON.stringify(val)));
         }
       },
       deep: true,
-    },
-    defaultFilter: {
-      handler(val) {
-        // check if the props default defaultFilter is still applied
-        if (!this.filter || this.filter.id === 'default') {
-          this.filter = {
-            ...val,
-            // if filter is changed from outside this often means resetting a filter so previous
-            // values should not be taken over (=leave second argument of function empty here)
-            filter_values: this.setFilterValues(val),
-          };
-        }
-      },
-      immediate: true,
     },
     /**
      * watch if applied filter changes from outside
@@ -1257,21 +1304,23 @@ export default {
        */
       handler(val) {
         // check if anything actually changed
-        if (JSON.stringify(val) !== JSON.stringify(this.filter)) {
-          const newFilter = val || this.defaultFilter;
-          this.filter = {
+        if (JSON.stringify(val) !== JSON.stringify(this.filter)
+          && hasData(val.filter_values) !== hasData(this.filter.filter_values)) {
+          let newFilter = JSON.parse(JSON.stringify(val || this.defaultFilter));
+          newFilter = {
             ...newFilter,
             // if filter is changed from outside this often means resetting a filter so previous
             // values should not be taken over (=leave second argument of function empty here)
             filter_values: newFilter.filter_values || this.setFilterValues(newFilter),
           };
+          this.filter = newFilter;
           // check if the new filter has values
-          if (val && val.filter_values) {
+          if (newFilter && newFilter.filter_values) {
             // distinguish between date and others to assign to correct variable
-            if (val.type.includes('date')) {
-              this.currentInput = val.filter_values;
-            } else if (val.type === 'text') {
-              this.currentInput = val.filter_values[0] || '';
+            if (newFilter.type.includes('date')) {
+              this.currentInput = newFilter.filter_values;
+            } else if (newFilter.type === 'text') {
+              this.currentInput = newFilter.filter_values[0] || '';
             } else {
               this.currentInput = '';
             }
@@ -1825,58 +1874,6 @@ export default {
 
     /** OTHERS */
 
-    /**
-     * function to set the correct values for filter.filter_values attribute
-     *
-     * @param {Filter} newFilter - the newly selected filter
-     * @param {Filter} previousFilter - the previously set filter
-
-     * @returns {?string|Array|Object} the correct value type for the filter type
-     */
-    setFilterValues(newFilter, previousFilter = {}) {
-      const previousFilterValues = previousFilter.filter_values;
-      const { type } = newFilter;
-      const freetextAllowed = newFilter.freetext_allowed;
-      if (type === 'date') {
-        // map the date from daterange to date if necessary
-        return previousFilter.type && previousFilter.type.includes('date')
-          && previousFilterValues ? previousFilterValues.date_from || previousFilterValues.date_to : '';
-      }
-      if (type === 'daterange') {
-        // check if it can be mapped from date to daterange
-        if (previousFilter.type.includes('date')) {
-          return {
-            date_from: previousFilterValues ? previousFilterValues.date_from || previousFilterValues || '' : '',
-            date_to: previousFilterValues && previousFilterValues.to ? previousFilterValues.to : '',
-          };
-        }
-        // else just return empty object
-        return {
-          date_from: '',
-          date_to: '',
-        };
-      }
-      // check if both are autocomplete chips filters
-      if (type === 'chips' && freetextAllowed && previousFilter.type === 'chips' && previousFilter.freetext_allowed) {
-        // check if new filter is a superset filter of the previous filter (=previous
-        // filter id is included in current filter 'subsets' property
-        if (newFilter.subsets
-          && newFilter.subsets.includes(previousFilter[this.identifierPropertyName.filter])) {
-          return previousFilterValues;
-        }
-        // if both are chips with freetext keep options without id (=not specific entries)
-        return previousFilterValues && previousFilterValues.length ? previousFilterValues
-          .filter(value => !value[this.identifierPropertyName.autocompleteOption]) : [];
-      }
-      // check if previous filter was text and new filter is autocomplete chips
-      if (previousFilter.type === 'text' && previousFilterValues && previousFilterValues.length
-        && !!previousFilterValues[0] && type === 'chips' && freetextAllowed) {
-        return [{
-          [this.labelPropertyName.autocompleteOption]: previousFilterValues[0],
-        }];
-      }
-      return [];
-    },
     /**
      * reset all filter row input and navigational variables
      */

src/components/BaseAdvancedSearch/BaseAdvancedSearch.vue
@@ -768,11 +768,23 @@ export default {
   },
   emits: ['search', 'fetch-autocomplete', 'fetch-form-autocomplete', 'update:applied-filters', 'update:form-filter-values', 'update:advanced-form-open'],
   setup() {
+    /** ADD FILTER ROW */
     /**
+     * set up a reference to the main search field, which we need to be able
+     * to focus the input field after adding a filter row
+     * @type {Readonly<ShallowRef<null|HTMLElement>>}
      */
-    const searchContainer = ref(null);
+    const mainSearch = useTemplateRef('mainSearch');
+
+    /**
+     * get the native HTML input element stored in the `inputElement` variable in BaseSearch
+     * @type {ComputedRef<HTMLElement|null>}
+     */
+    const input = computed(() => mainSearch.value?.baseSearch?.inputElement || null);
+
     /** INTERNAL ID */
     const internalId = useId();
+
     /** ACCESSIBILITY ANNOUNCEMENTS */
     /**
      * set up a reference to the element to be able to attach the announcements element
@@ -786,8 +798,10 @@ export default {
      */
     const { announcement } = useAnnouncer(searchContainer);
     return {
-      searchContainer,
       internalId,
+      mainSearch,
+      input,
+      searchContainer,
       announcement,
     };
   },
@@ -1039,7 +1053,7 @@ export default {
         // check if val is actually different from prop value
         if (JSON.stringify(val) !== JSON.stringify(this.appliedFilters.slice(1))) {
           // if yes - inform parent
-          this.$emit('update:applied-filters', [...val, this.mainFilter]);
+          this.$emit('update:applied-filters', JSON.parse(JSON.stringify([...val, this.mainFilter])));
         }
       },
       deep: true,
@@ -1061,7 +1075,7 @@ export default {
           && JSON.stringify(this.mainFilter) !== JSON.stringify(val[this.mainFilterIndex])) {
           [this.mainFilter] = JSON.parse(JSON.stringify(val.slice(-1)));
         } else if (!val || val < 1) {
-          this.mainFilter = { ...this.defaultFilter };
+          this.mainFilter = JSON.parse(JSON.stringify(this.defaultFilter));
         }
       },
       immediate: true,
@@ -1104,9 +1118,9 @@ export default {
        * @event update:applied-filters
        * @param {Filter[]} - the list of updated applied filters
        */
-      this.$emit('update:applied-filters', [...this.appliedFiltersInt, val]);
+      this.$emit('update:applied-filters', JSON.parse(JSON.stringify([...this.appliedFiltersInt, val])));
       // also emit updated form filter values at this point
-      this.$emit('update:form-filter-values', { ...this.formFilterValuesInt, default: this.mainFilter.filter_values });
+      this.$emit('update:form-filter-values', JSON.parse(JSON.stringify({ ...this.formFilterValuesInt, default: this.mainFilter.filter_values })));
     },
     /**
      * have formFilterValues in sync with parent to be able to set them from outside
@@ -1126,7 +1140,7 @@ export default {
            *  are available under the default property
            *
            */
-          this.$emit('update:form-filter-values', { ...val, default: this.mainFilter.filter_values });
+          this.$emit('update:form-filter-values', JSON.parse(JSON.stringify({ ...val, default: this.mainFilter.filter_values })));
         }
       },
       deep: true,
@@ -1343,7 +1357,6 @@ export default {
       // and reset the main filter
       this.mainFilter = {
         ...this.defaultFilter,
-        filter_values: null,
       };
       // and store the main filter to compare to later
       this.originalMainFilter = JSON.parse(JSON.stringify(this.mainFilter));
@@ -1351,14 +1364,11 @@ export default {
       if (triggerSearch) {
         this.search();
       }
-      // now focus new added row search input but wait until it is rendered
-      this.$nextTick(() => {
-        // get the correct field from all input fields of that element
-        const newSearchInputField = Array.from(this.$refs.mainSearch.$el
-          .getElementsByTagName('input'))
-          .find(element => element.id.includes('search-input'));
-        newSearchInputField.focus();
-      });
+      // make sure the main search input element was found
+      if (this.input) {
+        // and if yes focus the 'new' last row
+        this.input.focus();
+      }
     },
     /**
      * remove filter after 'x' was triggered

commit 8710cfb2c2aef5384593d16767e6bc565190601e

fix(BaseAdvancedSearch): filter sync between search row and parent not working
* caused by changes in commit a4564ce6

src/components/BaseAdvancedSearch/BaseAdvancedSearchRow.vue
@@ -875,8 +875,7 @@ export default {
        */
       handler(val) {
         // check if anything actually changed
-        if (JSON.stringify(val) !== JSON.stringify(this.filter)
-          && hasData(val.filter_values) !== hasData(this.filter.filter_values)) {
+        if (JSON.stringify(val) !== JSON.stringify(this.filter)) {
           let newFilter = JSON.parse(JSON.stringify(val || this.defaultFilter));
           newFilter = {
             ...newFilter,